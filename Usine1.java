e7c62173086cd270785dae033f1cd5e89ae71a06
LBTom <leberretom@gmail.com>
2018-12-20 18:04:56 +0100

    Question 2

--- Usine1.java
+++ Usine1.java
@@ -1,3 +1,10 @@
+/*@predicate tache(Tache tache; int temps_necessaire, int gain) = 
+	tache.temps_necessaire |-> temps_necessaire &*& 
+	tache.gain |-> gain &*&
+	temps_necessaire > 0 &*& 
+	gain > 0; 
+
+@*/
 class Tache
 {
 
@@ -6,18 +13,23 @@ class Tache
 	private int gain;
 	
 	public Tache(int t,int g)
+	//@requires t>0 && g>0;
+	//@ensures tache(this, t, g);
 	{
 		this.temps_necessaire = t;
 		this.gain = g;
 	}
 	
 	public int get_gain()
-
+	//@requires tache(this, ?temps_necessaire,?gain);
+	//@ensures tache(this, temps_necessaire, gain) &*& result == gain;
 	{
 		return this.gain;
 	}
 	
 	public int get_temps_necessaire()
+	//@requires tache(this, ?temps_necessaire, ?gain);
+	//@ensures tache(this, temps_necessaire, gain) &*& result == temps_necessaire;
 	{
 		return this.temps_necessaire;
 	}
